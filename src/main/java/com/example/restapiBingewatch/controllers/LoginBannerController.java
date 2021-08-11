2119dfca9d7232b396b5a678f3db9a1cccd8945b
RaakeshB <raakesh.balaji@gmail.com>
2021-08-11 03:14:36 -0400

    Update LoginBannerController.java

--- src/main/java/com/example/restapiBingewatch/controllers/LoginBannerController.java
+++ src/main/java/com/example/restapiBingewatch/controllers/LoginBannerController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
-@CrossOrigin("https://bingewatch-ott.herokuapp.com/")
+@CrossOrigin("*")
 @Controller
 public class LoginBannerController {
 
