33556b793f51890a3e5ed933a572fab187ff4d9d
RaakeshB <raakesh.balaji@gmail.com>
2021-08-11 03:15:15 -0400

    Update UserController.java

--- src/main/java/com/example/restapiBingewatch/controllers/UserController.java
+++ src/main/java/com/example/restapiBingewatch/controllers/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
-@CrossOrigin("https://bingewatch-ott.herokuapp.com/")
+@CrossOrigin("*")
 @Controller
 public class UserController {
 
