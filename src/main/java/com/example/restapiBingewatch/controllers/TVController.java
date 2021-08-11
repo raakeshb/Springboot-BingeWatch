ec31d0df85c6604b772f4ab8f657aaf0d3d8617a
RaakeshB <raakesh.balaji@gmail.com>
2021-08-11 03:15:02 -0400

    Update TVController.java

--- src/main/java/com/example/restapiBingewatch/controllers/TVController.java
+++ src/main/java/com/example/restapiBingewatch/controllers/TVController.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
-@CrossOrigin("https://bingewatch-ott.herokuapp.com/")
+@CrossOrigin("*")
 @Controller
 public class TVController {
 
